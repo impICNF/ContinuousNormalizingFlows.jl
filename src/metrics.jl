f70473fd0dcd7b69c38586856105ceee51ad1a92
Hossein Pourbozorg <prbzrg@gmail.com>
2021-12-20 04:22:37 +0330

    add metrics for conditionals

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,11 +1,11 @@
-export loglikelihood
+export agg_loglikelihood
 
-function loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
+function agg_loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
     logp̂x = inference(icnf, TestMode(), xs)
     agg(logp̂x)
 end
 
-function loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}, ys::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
+function agg_loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}, ys::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
     logp̂x = inference(icnf, TestMode(), xs, ys)
     agg(logp̂x)
 end
