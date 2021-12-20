06ec44bd1c5a7e547f7ad4efd7cb10901262a173
Hossein Pourbozorg <prbzrg@gmail.com>
2021-12-20 04:22:28 +0330

    better organization for smoke tests

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,4 +1,11 @@
+export loglikelihood
+
 function loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
     logp̂x = inference(icnf, TestMode(), xs)
+    agg(logp̂x)
+end
+
+function loglikelihood(icnf::AbstractICNF{T}, xs::AbstractMatrix{T}, ys::AbstractMatrix{T}; agg::Function=mean)::T where {T <: AbstractFloat}
+    logp̂x = inference(icnf, TestMode(), xs, ys)
     agg(logp̂x)
 end
