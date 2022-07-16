d7e83f94f766e7aeb045f920a35c479497051b16
Hossein Pourbozorg <prbzrg@gmail.com>
2022-07-16 18:26:11 +0430

    use better dist

--- src/cond_rnode.jl
+++ src/cond_rnode.jl
@@ -30,7 +30,7 @@ function CondRNODE{T}(
         nn,
         nvars::Integer,
         ;
-        basedist::Distribution=MvNormal(zeros(T, nvars), Diagonal(ones(T, nvars))),
+        basedist::Distribution=MvNormal(Zeros{T}(nvars), one(T)),
         tspan::Tuple{T, T}=convert(Tuple{T, T}, (0, 1)),
 
         solvealg_test::SciMLBase.AbstractODEAlgorithm=default_solvealg,
