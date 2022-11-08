99cc2ad5cf290489af452099867b2b8ab455c6af
Hossein Pourbozorg <prbzrg@gmail.com>
2022-11-08 08:08:08 +0330

    more tests (#97)

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,12 +16,23 @@ using ICNF,
     Tracker,
     Zygote
 
-CUDA.allowscalar() do
-    include("core.jl")
+include("core.jl")
 
-    @testset "Overall" begin
+@testset "Overall" begin
+    CUDA.allowscalar() do
         include("smoke_tests.jl")
     end
+end
 
-    # Aqua.test_all(ICNF)
+@testset "Quality" begin
+    Aqua.test_ambiguities(ICNF)
+    Aqua.test_unbound_args(ICNF)
+    Aqua.test_undefined_exports(ICNF)
+    Aqua.test_project_extras(ICNF)
+    Aqua.test_stale_deps(ICNF)
+    Aqua.test_deps_compat(ICNF)
+    Aqua.test_project_toml_formatting(ICNF)
+
+    @test isempty(Test.detect_ambiguities(ICNF; recursive=true))
+    @test isempty(Test.detect_unbound_args(ICNF; recursive=true))
 end
