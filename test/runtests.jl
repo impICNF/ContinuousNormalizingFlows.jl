07e5f67ab4d9f2555c6039ae1d11735aba8584e4
Hossein Pourbozorg <prbzrg@gmail.com>
2023-01-26 23:03:40 +0330

    separete quality tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,15 +32,6 @@ GROUP = get(ENV, "GROUP", "All")
     end
 end
 
-@testset "Quality" begin
-    Aqua.test_ambiguities(ICNF)
-    Aqua.test_unbound_args(ICNF)
-    Aqua.test_undefined_exports(ICNF)
-    Aqua.test_project_extras(ICNF)
-    Aqua.test_stale_deps(ICNF)
-    Aqua.test_deps_compat(ICNF)
-    Aqua.test_project_toml_formatting(ICNF)
-
-    @test isempty(Test.detect_ambiguities(ICNF; recursive = true))
-    @test isempty(Test.detect_unbound_args(ICNF; recursive = true))
+if GROUP == "All" || GROUP == "Quality"
+    include("quality_tests.jl")
 end
