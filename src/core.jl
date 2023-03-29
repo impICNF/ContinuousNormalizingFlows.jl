003ad1b1d9a2730268fffedf02dea1d6e27ed6f8
prbzrg <prbzrg@users.noreply.github.com>
2023-03-29 21:43:02 +0000

    Format .jl files

--- src/core.jl
+++ src/core.jl
@@ -1,10 +1,17 @@
 # SciML interface
 
 function callback_f(icnf::AbstractFlows, n::Integer)::Function
-    prgr = Progress(n; dt=eps(), desc="Training: ", showspeed=true)
+    prgr = Progress(n; dt = eps(), desc = "Training: ", showspeed = true)
     itr_n = 1
     function f(ps, l)
-        ProgressMeter.next!(prgr; showvalues = [(:loss_value, l), (:iteration, itr_n), (:last_update, Dates.now())])
+        ProgressMeter.next!(
+            prgr;
+            showvalues = [
+                (:loss_value, l),
+                (:iteration, itr_n),
+                (:last_update, Dates.now()),
+            ],
+        )
         itr_n += one(itr_n)
         false
     end
