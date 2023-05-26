583c22455577330f67723da1fb8e34cfb5a19850
Hossein Pourbozorg <prbzrg@gmail.com>
2023-05-26 16:35:59 +0330

    Name change (#189)
    * change the name to ContinuousNF
    
    * update files to new name
    
    * update

--- test/quality_tests.jl
+++ test/quality_tests.jl
@@ -1,12 +1,12 @@
 @testset "Quality" begin
-    Aqua.test_ambiguities(ICNF)
-    Aqua.test_unbound_args(ICNF)
-    Aqua.test_undefined_exports(ICNF)
-    Aqua.test_project_extras(ICNF)
-    Aqua.test_stale_deps(ICNF)
-    Aqua.test_deps_compat(ICNF)
-    Aqua.test_project_toml_formatting(ICNF)
+    Aqua.test_ambiguities(ContinuousNF)
+    Aqua.test_unbound_args(ContinuousNF)
+    Aqua.test_undefined_exports(ContinuousNF)
+    Aqua.test_project_extras(ContinuousNF)
+    Aqua.test_stale_deps(ContinuousNF)
+    Aqua.test_deps_compat(ContinuousNF)
+    Aqua.test_project_toml_formatting(ContinuousNF)
 
-    @test isempty(Test.detect_ambiguities(ICNF; recursive = true))
-    @test isempty(Test.detect_unbound_args(ICNF; recursive = true))
+    @test isempty(Test.detect_ambiguities(ContinuousNF; recursive = true))
+    @test isempty(Test.detect_unbound_args(ContinuousNF; recursive = true))
 end
