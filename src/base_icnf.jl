d96d67a3f499c158725700e8e64db139ea5514f6
Hossein Pourbozorg <prbzrg@gmail.com>
2023-06-08 05:08:37 +0330

    fix warning

--- src/base_icnf.jl
+++ src/base_icnf.jl
@@ -69,7 +69,7 @@ for arr_tp in (:Array, :CuArray, :AbstractArray)
             rng::AbstractRNG = Random.default_rng(),
             sol_args::Tuple = icnf.sol_args,
             sol_kwargs::Dict = icnf.sol_kwargs,
-        )::$arr_tp{T2, 1} where {T <: AbstractFloat, T2 <: Real}
+        )::$arr_tp{<:Real, 1} where {T <: AbstractFloat}
             n_aug = n_augment(icnf, mode)
             new_xs::$arr_tp = rand(rng, icnf.basedist)
             zrs::$arr_tp = zeros(T, n_aug + 1)
@@ -97,7 +97,7 @@ for arr_tp in (:Array, :CuArray, :AbstractArray)
             rng::AbstractRNG = Random.default_rng(),
             sol_args::Tuple = icnf.sol_args,
             sol_kwargs::Dict = icnf.sol_kwargs,
-        )::$arr_tp{T2, 2} where {T <: AbstractFloat, T2 <: Real}
+        )::$arr_tp{<:Real, 2} where {T <: AbstractFloat}
             n_aug = n_augment(icnf, mode)
             new_xs::$arr_tp = rand(rng, icnf.basedist, n)
             zrs::$arr_tp = zeros(T, n_aug + 1, size(new_xs, 2))
