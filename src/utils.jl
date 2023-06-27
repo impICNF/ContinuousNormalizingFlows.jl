652561d73c18bbce6ec3ecdc9b05c0738b469e7f
Hossein Pourbozorg <prbzrg@gmail.com>
2023-06-27 12:09:43 +0330

    update testmode jacs

--- src/utils.jl
+++ src/utils.jl
@@ -1,73 +1,20 @@
 function jacobian_batched(
     f,
-    xs::AbstractMatrix{<:AbstractFloat},
-    T::Type{<:AbstractFloat},
-    AT::Type{<:AbstractArray},
-    CM::Type{<:ZygoteMatrixMode},
-)::Tuple
-    y, back = Zygote.pullback(f, xs)
-    z::AT = zeros(T, size(xs))
-    res::AT = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= only(back(z))
-        z[i, :] .= zero(T)
-    end
-    y, res
-end
-
-function jacobian_batched(
-    f,
-    xs::AbstractMatrix{<:AbstractFloat},
-    T::Type{<:AbstractFloat},
-    AT::Type{<:AbstractArray},
-    CM::Type{<:SDVecJacMatrixMode},
-)::Tuple
-    y = f(xs)
-    z::AT = zeros(T, size(xs))
-    res::AT = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= reshape(auto_vecjac(f, xs, z), size(xs))
-        z[i, :] .= zero(T)
-    end
-    y, res
-end
-
-function jacobian_batched(
-    f,
-    xs::AbstractMatrix{<:AbstractFloat},
-    T::Type{<:AbstractFloat},
-    AT::Type{<:AbstractArray},
-    CM::Type{<:SDJacVecMatrixMode},
-)::Tuple
-    y = f(xs)
-    z::AT = zeros(T, size(xs))
-    res::AT = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= reshape(auto_jacvec(f, xs, z), size(xs))
-        z[i, :] .= zero(T)
-    end
-    y, res
-end
-
-function jacobian_batched(
-    f,
     xs::AbstractMatrix{<:Real},
     T::Type{<:AbstractFloat},
     AT::Type{<:AbstractArray},
     CM::Type{<:ZygoteMatrixMode},
 )::Tuple
     y, back = Zygote.pullback(f, xs)
-    z::AT = zeros(T, size(xs))
-    res::AT{Real} = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= only(back(z))
-        z[i, :] .= zero(T)
+    z = Zygote.Buffer(xs)
+    z[:, :] = convert(AT, zeros(T, size(xs)))
+    res = Zygote.Buffer(xs, size(xs, 1), size(xs, 1), size(xs, 2))
+    for i in axes(xs, 1)
+        z[i, :] = one(T)
+        res[i, :, :] = only(back(z))
+        z[i, :] = zero(T)
     end
-    y, res
+    y, copy(res)
 end
 
 function jacobian_batched(
@@ -78,14 +25,15 @@ function jacobian_batched(
     CM::Type{<:SDVecJacMatrixMode},
 )::Tuple
     y = f(xs)
-    z::AT = zeros(T, size(xs))
-    res::AT{Real} = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= reshape(auto_vecjac(f, xs, z), size(xs))
-        z[i, :] .= zero(T)
+    z = Zygote.Buffer(xs)
+    z[:, :] = convert(AT, zeros(T, size(xs)))
+    res = Zygote.Buffer(xs, size(xs, 1), size(xs, 1), size(xs, 2))
+    for i in axes(xs, 1)
+        z[i, :] = one(T)
+        res[i, :, :] = reshape(auto_vecjac(f, xs, z), size(xs))
+        z[i, :] = zero(T)
     end
-    y, res
+    y, copy(res)
 end
 
 function jacobian_batched(
@@ -96,12 +44,13 @@ function jacobian_batched(
     CM::Type{<:SDJacVecMatrixMode},
 )::Tuple
     y = f(xs)
-    z::AT = zeros(T, size(xs))
-    res::AT{Real} = zeros(T, size(xs, 1), size(xs, 1), size(xs, 2))
-    for i in axes(y, 1)
-        z[i, :] .= one(T)
-        res[i, :, :] .= reshape(auto_jacvec(f, xs, z), size(xs))
-        z[i, :] .= zero(T)
+    z = Zygote.Buffer(xs)
+    z[:, :] = convert(AT, zeros(T, size(xs)))
+    res = Zygote.Buffer(xs, size(xs, 1), size(xs, 1), size(xs, 2))
+    for i in axes(xs, 1)
+        z[i, :] = one(T)
+        res[i, :, :] = reshape(auto_jacvec(f, xs, z), size(xs))
+        z[i, :] = zero(T)
     end
-    y, res
+    y, copy(res)
 end
