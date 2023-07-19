91a62f9cc2650b1caf2f032b968d5304ad3203f2
Hossein Pourbozorg <prbzrg@gmail.com>
2023-07-19 11:26:11 +0330

    update quality test

--- test/quality_tests.jl
+++ test/quality_tests.jl
@@ -1,12 +1,3 @@
 @testset "Quality" begin
-    Aqua.test_ambiguities(ContinuousNormalizingFlows)
-    Aqua.test_unbound_args(ContinuousNormalizingFlows)
-    Aqua.test_undefined_exports(ContinuousNormalizingFlows)
-    Aqua.test_project_extras(ContinuousNormalizingFlows)
-    Aqua.test_stale_deps(ContinuousNormalizingFlows)
-    Aqua.test_deps_compat(ContinuousNormalizingFlows)
-    Aqua.test_project_toml_formatting(ContinuousNormalizingFlows)
-
-    @test isempty(Test.detect_ambiguities(ContinuousNormalizingFlows; recursive = true))
-    @test isempty(Test.detect_unbound_args(ContinuousNormalizingFlows; recursive = true))
+    Aqua.test_all(ContinuousNormalizingFlows)
 end
