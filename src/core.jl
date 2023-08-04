14df439b5c339476b22085918344bb81ffe96329
Hossein Pourbozorg <prbzrg@gmail.com>
2023-08-04 06:07:09 +0330

    fix callback_f

--- src/core.jl
+++ src/core.jl
@@ -1,11 +1,11 @@
 # SciML interface
 
-function callback_f(ps, l, icnf::AbstractFlows, prgr::Progress, itr_n::Integer)
+function callback_f(ps, l, icnf::AbstractFlows, prgr::Progress, itr_n::AbstractVector)
     ProgressMeter.next!(
         prgr;
         showvalues = [(:loss_value, l), (:iteration, itr_n), (:last_update, Dates.now())],
     )
-    itr_n[] += one(itr_n)
+    itr_n[] += one(itr_n[])
     false
 end
 
