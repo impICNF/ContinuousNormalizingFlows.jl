00003113c0ebb214aff2c4514e6942f7bf24dbb2
Hossein Pourbozorg <prbzrg@gmail.com>
2023-08-16 12:14:59 +0330

    fix benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,15 +6,6 @@ SUITE["main"] = BenchmarkGroup(["package", "simple"])
 SUITE["main"]["0-order"] = BenchmarkGroup(["direct"])
 SUITE["main"]["1-order"] = BenchmarkGroup(["gradient"])
 
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 2^13
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = convert(Float64, 2 * 60)
-BenchmarkTools.DEFAULT_PARAMETERS.evals = 1
-BenchmarkTools.DEFAULT_PARAMETERS.overhead = 0.0
-BenchmarkTools.DEFAULT_PARAMETERS.gctrial = true
-BenchmarkTools.DEFAULT_PARAMETERS.gcsample = false
-BenchmarkTools.DEFAULT_PARAMETERS.time_tolerance = convert(Float64, eps(Float16))
-BenchmarkTools.DEFAULT_PARAMETERS.memory_tolerance = convert(Float64, eps(Float16))
-
 nvars = 8
 n = 128
 rng = Random.default_rng()
@@ -30,6 +21,12 @@ grad_diff_loss_train() = Zygote.gradient(diff_loss_train, ps)
 grad_diff_loss_test() = Zygote.gradient(diff_loss_test, ps)
 t_loss_train() = loss(icnf, TrainMode(), r, ps, st)
 t_loss_test() = loss(icnf, TestMode(), r, ps, st)
+
+t_loss_train()
+t_loss_test()
+grad_diff_loss_train()
+grad_diff_loss_test()
+GC.gc()
 
 SUITE["main"]["0-order"]["train"] = @benchmarkable $t_loss_train()
 SUITE["main"]["0-order"]["test"] = @benchmarkable $t_loss_test()
