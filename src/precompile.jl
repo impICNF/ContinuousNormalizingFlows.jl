272fe6b12a22f24209a9e720f1cf0c8cc27a92c3
Hossein Pourbozorg <prbzrg@gmail.com>
2023-10-24 22:49:12 +0330

    precompile is single loop

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,11 +2,15 @@
     @compile_workload begin
         fllprcmpltn = @load_preference("fullprecompilation", false)
         mts =
-            fllprcmpltn ? Type{<:AbstractICNF}[RNODE, FFJORD, Planar] :
-            Type{<:AbstractICNF}[RNODE]
-        cmts =
-            fllprcmpltn ? Type{<:AbstractCondICNF}[CondRNODE, CondFFJORD, CondPlanar] :
-            Type{<:AbstractCondICNF}[]
+            fllprcmpltn ?
+            Type{<:AbstractFlows}[
+                RNODE,
+                FFJORD,
+                Planar,
+                CondRNODE,
+                CondFFJORD,
+                CondPlanar,
+            ] : Type{<:AbstractFlows}[RNODE]
         compute_modes =
             fllprcmpltn ?
             Type{<:ComputeMode}[
@@ -29,24 +33,6 @@
             if compute_mode <: MatrixMode
                 r = hcat(r)
             end
-            if mt <: Planar
-                nn = PlanarLayer(nvars, tanh)
-            else
-                nn = Lux.Dense(nvars => nvars, tanh)
-            end
-            icnf = construct(mt, nn, nvars; compute_mode)
-            ps, st = Lux.setup(icnf.rng, icnf)
-            ps = ComponentArray(ps)
-            L = loss(icnf, omode, r, ps, st)
-        end
-        for compute_mode in compute_modes, omode in omodes, mt in cmts
-            data_dist = Distributions.Beta{data_type}(
-                convert(Tuple{data_type, data_type}, (2, 4))...,
-            )
-            r = convert.(data_type, rand(data_dist, nvars))
-            if compute_mode <: MatrixMode
-                r = hcat(r)
-            end
             data_dist2 = Distributions.Beta{data_type}(
                 convert(Tuple{data_type, data_type}, (4, 2))...,
             )
@@ -54,15 +40,27 @@
             if compute_mode <: MatrixMode
                 r2 = hcat(r2)
             end
-            if mt <: CondPlanar
-                nn = PlanarLayer(nvars, tanh; n_cond = nvars)
+            if mt <: AbstractCondICNF
+                if mt <: CondPlanar
+                    nn = PlanarLayer(nvars, tanh; n_cond = nvars)
+                else
+                    nn = Lux.Dense(2 * nvars => nvars, tanh)
+                end
             else
-                nn = Lux.Dense(2 * nvars => nvars, tanh)
+                if mt <: Planar
+                    nn = PlanarLayer(nvars, tanh)
+                else
+                    nn = Lux.Dense(nvars => nvars, tanh)
+                end
             end
             icnf = construct(mt, nn, nvars; compute_mode)
             ps, st = Lux.setup(icnf.rng, icnf)
             ps = ComponentArray(ps)
-            L = loss(icnf, omode, r, r2, ps, st)
+            if mt <: AbstractCondICNF
+                L = loss(icnf, omode, r, r2, ps, st)
+            else
+                L = loss(icnf, omode, r, ps, st)
+            end
         end
     end
 end
