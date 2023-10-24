e56d862dc21739c17ea4fa9e448321c28ce3830c
Hossein Pourbozorg <prbzrg@gmail.com>
2023-10-24 22:04:31 +0330

    1st loop

--- test/call_tests.jl
+++ test/call_tests.jl
@@ -70,7 +70,9 @@
         data_dist =
             Distributions.Beta{data_type}(convert(Tuple{data_type, data_type}, (2, 4))...)
         r = convert.(data_type, rand(data_dist, nvars))
-        r_arr = hcat(r)
+        if compute_mode <: MatrixMode
+            r = hcat(r)
+        end
 
         if mt <: Planar
             nn = aug_steer ? PlanarLayer(nvars * 2, tanh) : PlanarLayer(nvars, tanh)
@@ -98,13 +100,16 @@
         ps = ComponentArrays.ComponentArray(ps)
         if resource isa ComputationalResources.CUDALibs
             r = gdev(r)
-            r_arr = gdev(r_arr)
             ps = gdev(ps)
             st = gdev(st)
         end
 
         @test !isnothing(inference(icnf, omode, r, ps, st))
-        @test !isnothing(generate(icnf, omode, ps, st))
+        if compute_mode <: MatrixMode
+            @test !isnothing(generate(icnf, omode, ps, st, 1))
+        else
+            @test !isnothing(generate(icnf, omode, ps, st))
+        end
 
         @test !isnothing(loss(icnf, omode, r, ps, st))
 
@@ -152,152 +157,6 @@
         # @test !isnothing(ForwardDiff.hessian(diff_loss, ps))
 
         diff2_loss(x) = loss(icnf, omode, x, ps, st)
-
-        @testset "$(typeof(adb).name.name) / Loss / x" for adb in adb_list
-            @test_throws MethodError !isnothing(
-                AbstractDifferentiation.derivative(adb, diff2_loss, r),
-            )
-            @test !isnothing(AbstractDifferentiation.gradient(adb, diff2_loss, r))
-            @test !isnothing(AbstractDifferentiation.jacobian(adb, diff2_loss, r))
-            # @test !isnothing(AbstractDifferentiation.hessian(adb, diff2_loss, r))
-        end
-
-        @test !isnothing(Zygote.gradient(diff2_loss, r))
-        @test !isnothing(Zygote.jacobian(diff2_loss, r))
-        # @test !isnothing(Zygote.diaghessian(diff2_loss, r))
-        # @test !isnothing(Zygote.hessian(diff2_loss, r))
-        # @test !isnothing(Zygote.hessian_reverse(diff2_loss, r))
-        diff2_loss2(x) = Zygote.checkpointed(diff2_loss, x)
-        @test !isnothing(Zygote.gradient(diff2_loss2, r))
-        @test !isnothing(Zygote.jacobian(diff2_loss2, r))
-        # @test !isnothing(Zygote.diaghessian(diff2_loss2, r))
-        # @test !isnothing(Zygote.hessian(diff2_loss2, r))
-        # @test !isnothing(Zygote.hessian_reverse(diff2_loss2, r))
-        diff2_loss3(x) = Zygote.forwarddiff(diff2_loss, x)
-        @test !isnothing(Zygote.gradient(diff2_loss3, r))
-        @test !isnothing(Zygote.jacobian(diff2_loss3, r))
-        # @test !isnothing(Zygote.diaghessian(diff2_loss3, r))
-        # @test !isnothing(Zygote.hessian(diff2_loss3, r))
-        # @test !isnothing(Zygote.hessian_reverse(diff2_loss3, r))
-        diff2_loss4(x) = Zygote.forwarddiff(diff2_loss2, x)
-        @test !isnothing(Zygote.gradient(diff2_loss4, r))
-        @test !isnothing(Zygote.jacobian(diff2_loss4, r))
-        # @test !isnothing(Zygote.diaghessian(diff2_loss4, r))
-        # @test !isnothing(Zygote.hessian(diff2_loss4, r))
-        # @test !isnothing(Zygote.hessian_reverse(diff2_loss4, r))
-
-        @test !isnothing(ReverseDiff.gradient(diff2_loss, r))
-        @test_throws MethodError !isnothing(ReverseDiff.jacobian(diff2_loss, r))
-        # @test !isnothing(ReverseDiff.hessian(diff2_loss, r))
-
-        @test !isnothing(ForwardDiff.gradient(diff2_loss, r))
-        @test_throws DimensionMismatch !isnothing(ForwardDiff.jacobian(diff2_loss, r))
-        # @test !isnothing(ForwardDiff.hessian(diff2_loss, r))
-
-        d = ICNFDist(icnf, omode, ps, st)
-
-        @test !isnothing(Distributions.logpdf(d, r))
-        @test !isnothing(Distributions.logpdf(d, r_arr))
-        @test !isnothing(Distributions.pdf(d, r))
-        @test !isnothing(Distributions.pdf(d, r_arr))
-        @test !isnothing(rand(d))
-        @test !isnothing(rand(d, 1))
-    end
-    @testset "$resource | $data_type | $compute_mode | inplace = $inplace | aug & steer = $aug_steer | nvars = $nvars | $omode | $mt" for resource in
-                                                                                                                                          resources,
-        data_type in data_types,
-        compute_mode in m_compute_modes,
-        inplace in inplaces,
-        aug_steer in aug_steers,
-        nvars in nvars_,
-        omode in omodes,
-        mt in mts
-
-        data_dist =
-            Distributions.Beta{data_type}(convert(Tuple{data_type, data_type}, (2, 4))...)
-        r = convert.(data_type, rand(data_dist, nvars))
-        r_arr = hcat(r)
-
-        if mt <: Planar
-            nn = aug_steer ? PlanarLayer(nvars * 2, tanh) : PlanarLayer(nvars, tanh)
-        else
-            nn =
-                aug_steer ? Lux.Dense(nvars * 2 => nvars * 2, tanh) :
-                Lux.Dense(nvars => nvars, tanh)
-        end
-        icnf =
-            aug_steer ?
-            construct(
-                mt,
-                nn,
-                nvars,
-                nvars;
-                data_type,
-                compute_mode,
-                inplace,
-                resource,
-                steer_rate = convert(data_type, 0.1),
-            ) : construct(mt, nn, nvars; data_type, compute_mode, inplace, resource)
-        icnf.sol_kwargs[:sensealg] = SciMLSensitivity.ForwardDiffSensitivity()
-        icnf.sol_kwargs[:verbose] = true
-        ps, st = Lux.setup(icnf.rng, icnf)
-        ps = ComponentArrays.ComponentArray(ps)
-        if resource isa ComputationalResources.CUDALibs
-            r = gdev(r)
-            r_arr = gdev(r_arr)
-            ps = gdev(ps)
-            st = gdev(st)
-        end
-
-        @test !isnothing(inference(icnf, omode, r_arr, ps, st))
-        @test !isnothing(generate(icnf, omode, ps, st, 1))
-
-        @test !isnothing(loss(icnf, omode, r_arr, ps, st))
-
-        diff_loss(x) = loss(icnf, omode, r_arr, x, st)
-
-        @testset "$(typeof(adb).name.name) / Loss / ps" for adb in adb_list
-            @test_throws MethodError !isnothing(
-                AbstractDifferentiation.derivative(adb, diff_loss, ps),
-            )
-            @test !isnothing(AbstractDifferentiation.gradient(adb, diff_loss, ps))
-            @test !isnothing(AbstractDifferentiation.jacobian(adb, diff_loss, ps))
-            # @test !isnothing(AbstractDifferentiation.hessian(adb, diff_loss, ps))
-        end
-
-        @test !isnothing(Zygote.gradient(diff_loss, ps))
-        @test !isnothing(Zygote.jacobian(diff_loss, ps))
-        # @test !isnothing(Zygote.diaghessian(diff_loss, ps))
-        # @test !isnothing(Zygote.hessian(diff_loss, ps))
-        # @test !isnothing(Zygote.hessian_reverse(diff_loss, ps))
-        diff_loss2(x) = Zygote.checkpointed(diff_loss, x)
-        @test !isnothing(Zygote.gradient(diff_loss2, ps))
-        @test !isnothing(Zygote.jacobian(diff_loss2, ps))
-        # @test !isnothing(Zygote.diaghessian(diff_loss2, ps))
-        # @test !isnothing(Zygote.hessian(diff_loss2, ps))
-        # @test !isnothing(Zygote.hessian_reverse(diff_loss2, ps))
-        # diff_loss3(x) = Zygote.forwarddiff(diff_loss, x)
-        # @test !isnothing(Zygote.gradient(diff_loss3, ps))
-        # @test !isnothing(Zygote.jacobian(diff_loss3, ps))
-        # @test !isnothing(Zygote.diaghessian(diff_loss3, ps))
-        # @test !isnothing(Zygote.hessian(diff_loss3, ps))
-        # @test !isnothing(Zygote.hessian_reverse(diff_loss3, ps))
-        # diff_loss4(x) = Zygote.forwarddiff(diff_loss2, x)
-        # @test !isnothing(Zygote.gradient(diff_loss4, ps))
-        # @test !isnothing(Zygote.jacobian(diff_loss4, ps))
-        # @test !isnothing(Zygote.diaghessian(diff_loss4, ps))
-        # @test !isnothing(Zygote.hessian(diff_loss4, ps))
-        # @test !isnothing(Zygote.hessian_reverse(diff_loss4, ps))
-
-        @test !isnothing(ReverseDiff.gradient(diff_loss, ps))
-        @test_throws MethodError !isnothing(ReverseDiff.jacobian(diff_loss, ps))
-        # @test !isnothing(ReverseDiff.hessian(diff_loss, ps))
-
-        @test !isnothing(ForwardDiff.gradient(diff_loss, ps))
-        @test_throws DimensionMismatch !isnothing(ForwardDiff.jacobian(diff_loss, ps))
-        # @test !isnothing(ForwardDiff.hessian(diff_loss, ps))
-
-        diff2_loss(x) = loss(icnf, omode, hcat(x), ps, st)
 
         @testset "$(typeof(adb).name.name) / Loss / x" for adb in adb_list
             @test_throws MethodError !isnothing(
