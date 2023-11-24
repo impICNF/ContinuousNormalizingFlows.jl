5924948d37dcdad40a6f514e4edb13a1414e6dfd
Hossein Pourbozorg <prbzrg@gmail.com>
2023-11-25 02:45:42 +0330

    more JET tests

--- test/instability_tests.jl
+++ test/instability_tests.jl
@@ -1,3 +1,14 @@
 @testset "Instability" begin
     JET.test_package("ContinuousNormalizingFlows")
+
+    n = 3
+    nvars = 7
+    r = rand(Float32, nvars, n)
+    nn = Lux.Dense(nvars => nvars, tanh)
+    icnf = construct(RNODE, nn, nvars; compute_mode = ZygoteMatrixMode)
+    ps, st = Lux.setup(icnf.rng, icnf)
+    ps = ComponentArrays.ComponentArray(ps)
+    loss(icnf, TrainMode(), r, ps, st)
+    JET.@test_call loss(icnf, TrainMode(), r, ps, st)
+    JET.@test_opt loss(icnf, TrainMode(), r, ps, st)
 end
