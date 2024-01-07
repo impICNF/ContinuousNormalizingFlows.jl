4b9c81a66abf5b8ef87b04405e03d226a19cb2ed
Hossein Pourbozorg <prbzrg@gmail.com>
2024-01-07 08:09:43 +0330

    merge FFJORD and RNODE

--- src/cond_ffjord.jl
+++ src/cond_ffjord.jl
@@ -34,11 +34,110 @@ struct CondFFJORD{
     rng::RNG
 end
 
+export CondRNODE
+
+"""
+Implementation of RNODE (Conditional Version)
+"""
+struct CondRNODE{
+    T <: AbstractFloat,
+    CM <: ComputeMode,
+    INPLACE,
+    AUGMENTED,
+    STEER,
+    NN <: LuxCore.AbstractExplicitLayer,
+    NVARS <: Int,
+    RESOURCE <: AbstractResource,
+    BASEDIST <: Distribution,
+    TSPAN <: NTuple{2, T},
+    STEERDIST <: Distribution,
+    DIFFERENTIATION_BACKEND <: AbstractDifferentiation.AbstractBackend,
+    AUTODIFF_BACKEND <: ADTypes.AbstractADType,
+    SOL_KWARGS <: NamedTuple,
+    RNG <: AbstractRNG,
+} <: AbstractCondICNF{T, CM, INPLACE, AUGMENTED, STEER}
+    nn::NN
+    nvars::NVARS
+    naugmented::NVARS
+
+    resource::RESOURCE
+    basedist::BASEDIST
+    tspan::TSPAN
+    steerdist::STEERDIST
+    differentiation_backend::DIFFERENTIATION_BACKEND
+    autodiff_backend::AUTODIFF_BACKEND
+    sol_kwargs::SOL_KWARGS
+    rng::RNG
+    λ₁::T
+    λ₂::T
+end
+
+function construct(
+    aicnf::Type{<:CondRNODE},
+    nn,
+    nvars::Int,
+    naugmented::Int = 0;
+    data_type::Type{<:AbstractFloat} = Float32,
+    compute_mode::Type{<:ComputeMode} = ADVecJacVectorMode,
+    inplace::Bool = false,
+    resource::AbstractResource = CPU1(),
+    basedist::Distribution = MvNormal(
+        Zeros{data_type}(nvars + naugmented),
+        Eye{data_type}(nvars + naugmented),
+    ),
+    tspan::NTuple{2} = (zero(data_type), one(data_type)),
+    steer_rate::AbstractFloat = zero(data_type),
+    differentiation_backend::AbstractDifferentiation.AbstractBackend = AbstractDifferentiation.ZygoteBackend(),
+    autodiff_backend::ADTypes.AbstractADType = ifelse(
+        compute_mode <: SDJacVecMatrixMode,
+        AutoForwardDiff(),
+        AutoZygote(),
+    ),
+    sol_kwargs::NamedTuple = sol_kwargs_defaults.medium,
+    rng::AbstractRNG = rng_AT(resource),
+    λ₁::AbstractFloat = convert(data_type, 1e-2),
+    λ₂::AbstractFloat = convert(data_type, 1e-2),
+)
+    steerdist = Uniform{data_type}(-steer_rate, steer_rate)
+
+    aicnf{
+        data_type,
+        compute_mode,
+        inplace,
+        !iszero(naugmented),
+        !iszero(steer_rate),
+        typeof(nn),
+        typeof(nvars),
+        typeof(resource),
+        typeof(basedist),
+        typeof(tspan),
+        typeof(steerdist),
+        typeof(differentiation_backend),
+        typeof(autodiff_backend),
+        typeof(sol_kwargs),
+        typeof(rng),
+    }(
+        nn,
+        nvars,
+        naugmented,
+        resource,
+        basedist,
+        tspan,
+        steerdist,
+        differentiation_backend,
+        autodiff_backend,
+        sol_kwargs,
+        rng,
+        λ₁,
+        λ₂,
+    )
+end
+
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:ADVecJacVectorMode},
+    icnf::Union{CondRNODE{T, <:ADVecJacVectorMode}, CondFFJORD{T, <:ADVecJacVectorMode}},
     mode::TrainMode,
     ys::AbstractVector{<:Real},
     ϵ::AbstractVector{T},
@@ -46,7 +145,7 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1)]
-    mz, VJ = AbstractDifferentiation.value_and_pullback_function(
+    ż, VJ = AbstractDifferentiation.value_and_pullback_function(
         icnf.differentiation_backend,
         let ys = ys, p = p, st = st
             x -> first(icnf.nn(vcat(x, ys), p, st))
@@ -54,15 +153,21 @@ end
         z,
     )
     ϵJ = only(VJ(ϵ))
-    trace_J = ϵJ ⋅ ϵ
-    vcat(mz, -trace_J)
+    l̇ = ϵJ ⋅ ϵ
+    if icnf isa CondRNODE
+        Ė = norm(ż)
+        ṅ = norm(ϵJ)
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:ADJacVecVectorMode},
+    icnf::Union{CondRNODE{T, <:ADJacVecVectorMode}, CondFFJORD{T, <:ADJacVecVectorMode}},
     mode::TrainMode,
     ys::AbstractVector{<:Real},
     ϵ::AbstractVector{T},
@@ -70,24 +175,30 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1)]
-    mz_JV = AbstractDifferentiation.value_and_pushforward_function(
+    ż_JV = AbstractDifferentiation.value_and_pushforward_function(
         icnf.differentiation_backend,
         let ys = ys, p = p, st = st
             x -> first(icnf.nn(vcat(x, ys), p, st))
         end,
         z,
     )
-    mz, Jϵ = mz_JV(ϵ)
+    ż, Jϵ = ż_JV(ϵ)
     Jϵ = only(Jϵ)
-    trace_J = ϵ ⋅ Jϵ
-    vcat(mz, -trace_J)
+    l̇ = ϵ ⋅ Jϵ
+    if icnf isa CondRNODE
+        Ė = norm(ż)
+        ṅ = norm(Jϵ)
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:ZygoteVectorMode},
+    icnf::Union{CondRNODE{T, <:ZygoteVectorMode}, CondFFJORD{T, <:ZygoteVectorMode}},
     mode::TrainMode,
     ys::AbstractVector{<:Real},
     ϵ::AbstractVector{T},
@@ -95,19 +206,25 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1)]
-    mz, VJ = Zygote.pullback(let ys = ys, p = p, st = st
+    ż, VJ = Zygote.pullback(let ys = ys, p = p, st = st
         x -> first(icnf.nn(vcat(x, ys), p, st))
     end, z)
     ϵJ = only(VJ(ϵ))
-    trace_J = ϵJ ⋅ ϵ
-    vcat(mz, -trace_J)
+    l̇ = ϵJ ⋅ ϵ
+    if icnf isa CondRNODE
+        Ė = norm(ż)
+        ṅ = norm(ϵJ)
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:SDVecJacMatrixMode},
+    icnf::Union{CondRNODE{T, <:SDVecJacMatrixMode}, CondFFJORD{T, <:SDVecJacMatrixMode}},
     mode::TrainMode,
     ys::AbstractMatrix{<:Real},
     ϵ::AbstractMatrix{T},
@@ -115,7 +232,7 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1), :]
-    mz = first(icnf.nn(vcat(z, ys), p, st))
+    ż = first(icnf.nn(vcat(z, ys), p, st))
     Jf = VecJac(
         let ys = ys, p = p, st = st
             x -> first(icnf.nn(vcat(x, ys), p, st))
@@ -124,15 +241,21 @@ end
         autodiff = icnf.autodiff_backend,
     )
     ϵJ = reshape(Jf * ϵ, size(z))
-    trace_J = sum(ϵJ .* ϵ; dims = 1)
-    vcat(mz, -trace_J)
+    l̇ = sum(ϵJ .* ϵ; dims = 1)
+    if icnf isa CondRNODE
+        Ė = transpose(norm.(eachcol(ż)))
+        ṅ = transpose(norm.(eachcol(ϵJ)))
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:SDJacVecMatrixMode},
+    icnf::Union{CondRNODE{T, <:SDJacVecMatrixMode}, CondFFJORD{T, <:SDJacVecMatrixMode}},
     mode::TrainMode,
     ys::AbstractMatrix{<:Real},
     ϵ::AbstractMatrix{T},
@@ -140,7 +263,7 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1), :]
-    mz = first(icnf.nn(vcat(z, ys), p, st))
+    ż = first(icnf.nn(vcat(z, ys), p, st))
     Jf = JacVec(
         let ys = ys, p = p, st = st
             x -> first(icnf.nn(vcat(x, ys), p, st))
@@ -149,15 +272,21 @@ end
         autodiff = icnf.autodiff_backend,
     )
     Jϵ = reshape(Jf * ϵ, size(z))
-    trace_J = sum(ϵ .* Jϵ; dims = 1)
-    vcat(mz, -trace_J)
+    l̇ = sum(ϵ .* Jϵ; dims = 1)
+    if icnf isa CondRNODE
+        Ė = transpose(norm.(eachcol(ż)))
+        ṅ = transpose(norm.(eachcol(Jϵ)))
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:ZygoteMatrixMode},
+    icnf::Union{CondRNODE{T, <:ZygoteMatrixMode}, CondFFJORD{T, <:ZygoteMatrixMode}},
     mode::TrainMode,
     ys::AbstractMatrix{<:Real},
     ϵ::AbstractMatrix{T},
@@ -165,12 +294,18 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1), :]
-    mz, VJ = Zygote.pullback(let ys = ys, p = p, st = st
+    ż, VJ = Zygote.pullback(let ys = ys, p = p, st = st
         x -> first(icnf.nn(vcat(x, ys), p, st))
     end, z)
     ϵJ = only(VJ(ϵ))
-    trace_J = sum(ϵJ .* ϵ; dims = 1)
-    vcat(mz, -trace_J)
+    l̇ = sum(ϵJ .* ϵ; dims = 1)
+    if icnf isa CondRNODE
+        Ė = transpose(norm.(eachcol(ż)))
+        ṅ = transpose(norm.(eachcol(ϵJ)))
+        vcat(ż, -l̇, Ė, ṅ)
+    else
+        vcat(ż, -l̇)
+    end
 end
 
 @views function augmented_f(
@@ -178,7 +313,10 @@ end
     u::Any,
     p::Any,
     t::Any,
-    icnf::CondFFJORD{T, <:ZygoteMatrixModeInplace, true},
+    icnf::Union{
+        CondRNODE{T, <:ZygoteMatrixModeInplace, true},
+        CondFFJORD{T, <:ZygoteMatrixModeInplace, true},
+    },
     mode::TrainMode,
     ys::AbstractMatrix{<:Real},
     ϵ::AbstractMatrix{T},
@@ -186,11 +324,43 @@ end
 ) where {T <: AbstractFloat}
     n_aug = n_augment(icnf, mode)
     z = u[begin:(end - n_aug - 1), :]
-    mz, VJ = Zygote.pullback(let ys = ys, p = p, st = st
+    ż, VJ = Zygote.pullback(let ys = ys, p = p, st = st
         x -> first(icnf.nn(vcat(x, ys), p, st))
     end, z)
     ϵJ = only(VJ(ϵ))
-    du[begin:(end - n_aug - 1), :] .= mz
+    du[begin:(end - n_aug - 1), :] .= ż
     du[(end - n_aug), :] .= -vec(sum(ϵJ .* ϵ; dims = 1))
+    if icnf isa CondRNODE
+        du[(end - n_aug + 1), :] .= norm.(eachcol(ż))
+        du[(end - n_aug + 2), :] .= norm.(eachcol(ϵJ))
+    end
     nothing
+end
+
+@inline function loss(
+    icnf::CondRNODE{<:AbstractFloat, <:VectorMode},
+    mode::TrainMode,
+    xs::AbstractVector{<:Real},
+    ys::AbstractVector{<:Real},
+    ps::Any,
+    st::Any,
+)
+    logp̂x, (Ė, ṅ) = inference(icnf, mode, xs, ys, ps, st)
+    -logp̂x + icnf.λ₁ * Ė + icnf.λ₂ * ṅ
+end
+
+@inline function loss(
+    icnf::CondRNODE{<:AbstractFloat, <:MatrixMode},
+    mode::TrainMode,
+    xs::AbstractMatrix{<:Real},
+    ys::AbstractMatrix{<:Real},
+    ps::Any,
+    st::Any,
+)
+    logp̂x, (Ė, ṅ) = inference(icnf, mode, xs, ys, ps, st)
+    mean(-logp̂x + icnf.λ₁ * Ė + icnf.λ₂ * ṅ)
+end
+
+@inline function n_augment(::CondRNODE, ::TrainMode)
+    2
 end
