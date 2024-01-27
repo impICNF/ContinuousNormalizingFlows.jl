06809b31cf5870e92f72b646a4e3c22a7382ce26
Hossein Pourbozorg <prbzrg@gmail.com>
2024-01-27 16:37:08 +0330

    update JET tests

--- test/instability_tests.jl
+++ test/instability_tests.jl
@@ -1,14 +1,18 @@
 @testset "Instability" begin
-    JET.test_package("ContinuousNormalizingFlows"; mode = :sound)
+    JET.test_package("ContinuousNormalizingFlows"; target_defined_modules = true)
 
-    n = 3
-    nvars = 7
-    r = rand(Float32, nvars, n)
+    nvars = 2
+    r = rand(Float32, nvars)
     nn = Lux.Dense(nvars => nvars, tanh)
-    icnf = construct(RNODE, nn, nvars; compute_mode = ZygoteMatrixMode)
+    icnf = construct(
+        RNODE,
+        nn,
+        nvars;
+        sol_kwargs = ContinuousNormalizingFlows.sol_kwargs_defaults.medium_noad,
+    )
     ps, st = Lux.setup(icnf.rng, icnf)
     ps = ComponentArrays.ComponentArray(ps)
     loss(icnf, TrainMode(), r, ps, st)
-    JET.@test_call mode = :sound loss(icnf, TrainMode(), r, ps, st)
-    JET.@test_opt mode = :sound loss(icnf, TrainMode(), r, ps, st)
+    JET.@test_call target_defined_modules = true loss(icnf, TrainMode(), r, ps, st)
+    JET.@test_opt target_defined_modules = true loss(icnf, TrainMode(), r, ps, st)
 end
