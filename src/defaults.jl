babc3d4a797d6bb3e0fdcbd82731473dc00ce5a8
Hossein Pourbozorg <prbzrg@gmail.com>
2024-02-23 17:17:30 +0330

    change default alg

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,16 +1,14 @@
 const sol_kwargs_defaults = (
     high = (
-        alg_hints = [:nonstiff],
-        alg = VCABM(),
+        alg = Vern6(),
         sensealg = QuadratureAdjoint(; autodiff = true, autojacvec = ZygoteVJP()),
         reltol = eps(one(Float32)),
         abstol = eps(one(Float32)) * eps(one(Float32)),
         maxiters = typemax(Int32),
     ),
     medium = (
-        alg_hints = [:nonstiff, :memorybound],
         save_everystep = false,
-        alg = VCABM(),
+        alg = Vern6(),
         sensealg = InterpolatingAdjoint(;
             autodiff = true,
             autojacvec = ZygoteVJP(),
@@ -21,17 +19,15 @@ const sol_kwargs_defaults = (
         maxiters = typemax(Int32),
     ),
     medium_noad = (
-        alg_hints = [:nonstiff, :memorybound],
         save_everystep = false,
-        alg = VCABM(),
+        alg = Vern6(),
         reltol = sqrt(eps(one(Float32))),
         abstol = eps(one(Float32)),
         maxiters = typemax(Int32),
     ),
     low = (
-        alg_hints = [:nonstiff, :memorybound],
         save_everystep = false,
-        alg = VCABM(),
+        alg = Vern6(),
         sensealg = InterpolatingAdjoint(;
             autodiff = true,
             autojacvec = ZygoteVJP(),
