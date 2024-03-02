f2e94100e7b20e8c86f0d376755484bd9801c169
Hossein Pourbozorg <prbzrg@gmail.com>
2024-03-02 11:32:21 +0330

    test `VCABM`

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,6 +1,6 @@
 const sol_kwargs_defaults = (
     high = (
-        alg = Tsit5(; thread = OrdinaryDiffEq.True()),
+        alg = VCABM(),
         sensealg = QuadratureAdjoint(; autodiff = true, autojacvec = ZygoteVJP()),
         reltol = eps(one(Float32)),
         abstol = eps(one(Float32)) * eps(one(Float32)),
@@ -8,7 +8,7 @@ const sol_kwargs_defaults = (
     ),
     medium = (
         save_everystep = false,
-        alg = Tsit5(; thread = OrdinaryDiffEq.True()),
+        alg = VCABM(),
         sensealg = InterpolatingAdjoint(;
             autodiff = true,
             autojacvec = ZygoteVJP(),
@@ -20,14 +20,14 @@ const sol_kwargs_defaults = (
     ),
     medium_noad = (
         save_everystep = false,
-        alg = Tsit5(; thread = OrdinaryDiffEq.True()),
+        alg = VCABM(),
         reltol = sqrt(eps(one(Float32))),
         abstol = eps(one(Float32)),
         maxiters = typemax(Int32),
     ),
     low = (
         save_everystep = false,
-        alg = Tsit5(; thread = OrdinaryDiffEq.True()),
+        alg = VCABM(),
         sensealg = InterpolatingAdjoint(;
             autodiff = true,
             autojacvec = ZygoteVJP(),
