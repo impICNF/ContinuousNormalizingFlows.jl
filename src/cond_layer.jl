8e9320b39d26ae5b2e514a7d3194ac1c798b1ad2
Hossein Pourbozorg <prbzrg@gmail.com>
2024-03-26 01:14:08 +0330

    use `LuxCore.apply`

--- src/cond_layer.jl
+++ src/cond_layer.jl
@@ -5,5 +5,5 @@ struct CondLayer{NN <: LuxCore.AbstractExplicitLayer, AT <: AbstractArray} <:
 end
 
 @inline function (m::CondLayer)(z::AbstractArray, ps::Any, st::Any)
-    m.nn(vcat(z, m.ys), ps, st)
+    LuxCore.apply(m.nn, vcat(z, m.ys), ps, st)
 end
