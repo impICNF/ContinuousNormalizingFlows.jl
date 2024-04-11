b776ad2aac5acdfac58c46d25998c377fbafad69
Hossein Pourbozorg <prbzrg@gmail.com>
2024-04-11 18:09:13 +0330

    fix jac

--- src/utils.jl
+++ src/utils.jl
@@ -1,13 +1,17 @@
-function jacobian_batched(
+@inline function jacobian_batched(
     icnf::AbstractICNF{T, <:DIMatrixMode},
     f::Function,
     xs::AbstractMatrix{<:Real},
 ) where {T}
     y, J = value_and_jacobian(f, icnf.autodiff_backend, xs)
-    y, split_jac(J)
+    y, split_jac(J, size(xs, 1))
 end
 
-function split_jac(x::AbstractMatrix{<:Real})
-    sz = convert(Int, sqrt(size(x, 1)))
-    (x[i:(i + sz - 1), i:(i + sz - 1)] for i in firstindex(x, 1):sz:lastindex(x, 1))
+@inline function split_jac(x::AbstractMatrix{<:Real}, sz::Integer)
+    (
+        x[i:j, i:j] for (i, j) in zip(
+            firstindex(x, 1):sz:lastindex(x, 1),
+            (firstindex(x, 1) + sz - 1):sz:lastindex(x, 1),
+        )
+    )
 end
