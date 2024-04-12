88a7de4792e54318f1f8640eee8541231451f5c5
Hossein Pourbozorg <prbzrg@gmail.com>
2024-04-12 11:13:52 +0330

    jacobian_batched for DIVecJacMatrixMode

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,21 @@
 @inline function jacobian_batched(
+    icnf::AbstractICNF{T, <:DIVecJacMatrixMode},
+    f::Function,
+    xs::AbstractMatrix{<:Real},
+) where {T}
+    y, VJ = DifferentiationInterface.value_and_pullback_split(f, icnf.autodiff_backend, xs)
+    z = similar(xs)
+    @ignore_derivatives fill!(z, zero(T))
+    res = Zygote.Buffer(xs, size(xs, 1), size(xs, 1), size(xs, 2))
+    for i in axes(xs, 1)
+        @ignore_derivatives z[i, :] .= one(T)
+        res[i, :, :] = VJ(z)
+        @ignore_derivatives z[i, :] .= zero(T)
+    end
+    y, copy(res)
+end
+
+@inline function jacobian_batched(
     icnf::AbstractICNF{T, <:DIMatrixMode},
     f::Function,
     xs::AbstractMatrix{<:Real},
