b45e114c2f168a7f27d65c7a0779c63571d166a2
Hossein Pourbozorg <prbzrg@gmail.com>
2024-05-15 03:21:47 +0330

    fix planar error (#414)
    * fix planar error
    
    * fix ambiguity
    
    * try new
    
    * fix

--- src/layers/planar_layer.jl
+++ src/layers/planar_layer.jl
@@ -71,34 +71,33 @@ function LuxCore.outputsize(m::PlanarLayer)
 end
 
 @inline function (m::PlanarLayer{true})(z::AbstractVector, ps::Any, st::NamedTuple)
-    ps.u * Lux.apply_bias_activation(m.activation, LinearAlgebra.dot(ps.w, z), only(ps.b)),
-    st
+    ps.u * m.activation.(LinearAlgebra.dot(ps.w, z) + only(ps.b)), st
 end
 
 @inline function (m::PlanarLayer{true})(z::AbstractMatrix, ps::Any, st::NamedTuple)
-    ps.u * Lux.apply_bias_activation(m.activation, transpose(ps.w) * z, only(ps.b)), st
+    ps.u * m.activation.(muladd(transpose(ps.w), z, only(ps.b))), st
 end
 
 @inline function (m::PlanarLayer{false})(z::AbstractVector, ps::Any, st::NamedTuple)
-    ps.u * Lux.apply_activation(m.activation, LinearAlgebra.dot(ps.w, z)), st
+    ps.u * m.activation.(LinearAlgebra.dot(ps.w, z)), st
 end
 
 @inline function (m::PlanarLayer{false})(z::AbstractMatrix, ps::Any, st::NamedTuple)
-    ps.u * Lux.apply_activation(m.activation, transpose(ps.w) * z), st
+    ps.u * m.activation.(transpose(ps.w) * z), st
 end
 
 @inline function pl_h(m::PlanarLayer{true}, z::AbstractVector, ps::Any, st::NamedTuple)
-    Lux.apply_bias_activation(m.activation, LinearAlgebra.dot(ps.w, z), only(ps.b)), st
+    m.activation.(LinearAlgebra.dot(ps.w, z) + only(ps.b)), st
 end
 
 @inline function pl_h(m::PlanarLayer{true}, z::AbstractMatrix, ps::Any, st::NamedTuple)
-    Lux.apply_bias_activation(m.activation, transpose(ps.w) * z, only(ps.b)), st
+    m.activation.(muladd(transpose(ps.w), z, only(ps.b))), st
 end
 
 @inline function pl_h(m::PlanarLayer{false}, z::AbstractVector, ps::Any, st::NamedTuple)
-    Lux.apply_activation(m.activation, LinearAlgebra.dot(ps.w, z)), st
+    m.activation.(LinearAlgebra.dot(ps.w, z)), st
 end
 
 @inline function pl_h(m::PlanarLayer{false}, z::AbstractMatrix, ps::Any, st::NamedTuple)
-    Lux.apply_activation(m.activation, transpose(ps.w) * z), st
+    m.activation.(transpose(ps.w) * z), st
 end
