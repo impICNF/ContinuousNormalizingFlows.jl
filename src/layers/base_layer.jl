2fbd26ec872e510dd71529ac03d3a93045898a33
Hossein Pourbozorg <prbzrg@gmail.com>
2024-05-14 23:28:03 +0330

    fix ambiguity

--- src/layers/base_layer.jl
+++ src/layers/base_layer.jl
@@ -1,11 +1,11 @@
-@inline function apply_act(::typeof(identity), x::Any)
+@inline function apply_act(::typeof(identity), x::Union{Number, AbstractArray})
     x
 end
 
-@inline function apply_act(activation::Any, x::Number)
+@inline function apply_act(activation::Function, x::Number)
     activation(x)
 end
 
-@inline function apply_act(activation::Any, x::AbstractArray)
+@inline function apply_act(activation::Function, x::AbstractArray)
     activation.(x)
 end
