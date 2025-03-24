86848743b6751aaca3e5f5a246598be56f4d249f
Hossein Pourbozorg <prbzrg@gmail.com>
2025-03-25 02:56:07 +0330

    outer AD

--- test/call_tests.jl
+++ test/call_tests.jl
@@ -29,15 +29,17 @@ Test.@testset "Call Tests" begin
     nvars_ = Int[2]
     aug_steers = Bool[false, true]
     inplaces = Bool[false, true]
-    adtypes = ADTypes.AbstractADType[ADTypes.AutoZygote(),
-    # ADTypes.AutoEnzyme(;
-    #     mode = Enzyme.set_runtime_activity(Enzyme.Reverse),
-    #     function_annotation = Enzyme.Const,
-    # ),
-    # ADTypes.AutoEnzyme(;
-    #     mode = Enzyme.set_runtime_activity(Enzyme.Forward),
-    #     function_annotation = Enzyme.Const,
-    # ),
+    adtypes = ADTypes.AbstractADType[
+        ADTypes.AutoZygote(),
+        ADTypes.AutoForwardDiff(),
+        # ADTypes.AutoEnzyme(;
+        #     mode = Enzyme.set_runtime_activity(Enzyme.Reverse),
+        #     function_annotation = Enzyme.Const,
+        # ),
+        # ADTypes.AutoEnzyme(;
+        #     mode = Enzyme.set_runtime_activity(Enzyme.Forward),
+        #     function_annotation = Enzyme.Const,
+        # ),
     ]
     compute_modes = ContinuousNormalizingFlows.ComputeMode[
         ContinuousNormalizingFlows.LuxVecJacMatrixMode(ADTypes.AutoZygote()),
