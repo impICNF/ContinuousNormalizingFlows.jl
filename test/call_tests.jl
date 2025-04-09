3a69c115d89b85238f94aea33dfbf31958c44043
Hossein Pourbozorg <prbzrg@gmail.com>
2025-04-09 05:32:42 +0330

    comment enzyme forward mode

--- test/call_tests.jl
+++ test/call_tests.jl
@@ -53,26 +53,26 @@ Test.@testset "Call Tests" begin
                 function_annotation = Enzyme.Const,
             ),
         ),
-        ContinuousNormalizingFlows.DIJacVecVectorMode(
-            ADTypes.AutoEnzyme(;
-                mode = Enzyme.Forward,
-                # mode = Enzyme.set_runtime_activity(Enzyme.Forward),
-                function_annotation = Enzyme.Const,
-            ),
-        ),
+        # ContinuousNormalizingFlows.DIJacVecVectorMode(
+        #     ADTypes.AutoEnzyme(;
+        #         mode = Enzyme.Forward,
+        #         # mode = Enzyme.set_runtime_activity(Enzyme.Forward),
+        #         function_annotation = Enzyme.Const,
+        #     ),
+        # ),
         ContinuousNormalizingFlows.DIVecJacMatrixMode(
             ADTypes.AutoEnzyme(;
                 mode = Enzyme.set_runtime_activity(Enzyme.Reverse),
                 function_annotation = Enzyme.Const,
             ),
         ),
-        ContinuousNormalizingFlows.DIJacVecMatrixMode(
-            ADTypes.AutoEnzyme(;
-                mode = Enzyme.Forward,
-                # mode = Enzyme.set_runtime_activity(Enzyme.Forward),
-                function_annotation = Enzyme.Const,
-            ),
-        ),
+        # ContinuousNormalizingFlows.DIJacVecMatrixMode(
+        #     ADTypes.AutoEnzyme(;
+        #         mode = Enzyme.Forward,
+        #         # mode = Enzyme.set_runtime_activity(Enzyme.Forward),
+        #         function_annotation = Enzyme.Const,
+        #     ),
+        # ),
     ]
     data_types = Type{<:AbstractFloat}[Float32]
     devices = MLDataDevices.AbstractDevice[MLDataDevices.cpu_device()]
