87e6ee7116d7c03146b159ff2445ef5e34ce2609
prbzrg <79970151+prbzrg@users.noreply.github.com>
2026-01-29 12:58:33 +0000

    Format .jl files

--- test/ci_tests/smoke_tests.jl
+++ test/ci_tests/smoke_tests.jl
@@ -202,6 +202,7 @@ Test.@testset verbose = true showtiming = true failfast = false "Smoke Tests" be
 
         Test.@testset verbose = true showtiming = true failfast = false "$adtype on loss" for adtype in
                                                                                               adtypes
+
             Test.@test !isnothing(DifferentiationInterface.gradient(diff_loss, adtype, ps)) broken =
                 compute_mode.adback isa ADTypes.AutoEnzyme{<:Enzyme.ForwardMode} && (
                     omode isa ContinuousNormalizingFlows.TrainMode || (
