b43831e1872357f085779b3ed86b8335b4e9c130
Hossein Pourbozorg <prbzrg@gmail.com>
2026-02-16 17:52:08 +0330

    fix

--- src/layers/cond_layer.jl
+++ src/layers/cond_layer.jl
@@ -4,6 +4,28 @@ struct CondLayer{NN <: LuxCore.AbstractLuxLayer, AT <: Any} <:
     ys::AT
 end
 
-function (m::CondLayer)(z::AbstractVecOrMat, ps::Any, st::NamedTuple)
+function (m::CondLayer{<:LuxCore.AbstractLuxLayer, <:AbstractArray})(
+    z::AbstractVecOrMat,
+    ps::Any,
+    st::NamedTuple,
+)
     return LuxCore.apply(m.nn, vcat(z, m.ys), ps, st)
+end
+
+function (m::CondLayer{<:LuxCore.AbstractLuxLayer, <:Number})(
+    z::AbstractVector,
+    ps::Any,
+    st::NamedTuple,
+)
+    return LuxCore.apply(m.nn, vcat(z, m.ys), ps, st)
+end
+
+function (m::CondLayer{<:LuxCore.AbstractLuxLayer, <:Number})(
+    z::AbstractMatrix,
+    ps::Any,
+    st::NamedTuple,
+)
+    ts = similar(z, 1, size(z, 2))
+    ChainRulesCore.@ignore_derivatives fill!(ts, m.ys)
+    return LuxCore.apply(m.nn, vcat(z, ts), ps, st)
 end
